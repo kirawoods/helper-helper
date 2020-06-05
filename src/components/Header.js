fd8982b6b7c701330e57134ec1b9aa6a7a0f69b7
Kira <kawoods826@gmail.com>
2020-06-04 22:28:00 -0700

    updates front page styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Header.css";
+import logo from "../images/logo.png";
 
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img className="header-logo" src={logo} />
+      <div className="menu">
+        <div className="menu-item">Post a Request</div>
+        <div className="menu-item">Help a Helper</div>
+      </div>
+    </div>
+  );
 }
