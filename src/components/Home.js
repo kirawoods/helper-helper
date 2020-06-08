07ecd7fccef6999b27de5564ea72de99ef6692b8
Kira <kawoods826@gmail.com>
2020-06-08 10:30:13 -0700

    adds How it Works section to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,6 +17,26 @@ export function Home() {
           essential workers and empower volunteers to make a difference.{" "}
         </p>
       </div>
+      <div className="how-it-works-section">
+        <h2 className="how-it-works-header">How It Works</h2>
+        <p>For Essential Workers</p>
+        <p>
+          Essential workers can post a request through the "Post a Request
+          Page". Their request will then show up on the "Helper Helper" page so
+          that volunteers can select a request to fulfill. You can make requests
+          for assistance with tasks that happen in person, such as grocery
+          delivery, or for tasks that can be done remotely, such as tax
+          preparation. Please remember to keep your requests in accordance with
+          local shelter in place ordinances.
+        </p>
+        <p>For Volunteers</p>
+        <p>
+          Volunteers can go to the "Help a Helper" page to view requests in
+          their area or look for remote opportunities. Click the "Fulfill
+          Request" button to respond to a helper request and get in contact with
+          the essential worker who made the request.
+        </p>
+      </div>
     </div>
   );
 }
