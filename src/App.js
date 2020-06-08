05f8314b178065a2b33fe13280da1fe73325ef58
Kira <kawoods826@gmail.com>
2020-06-07 17:31:41 -0700

    updates routes and adds about info to home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header";
+import { Home } from "././components/Home";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <Router>
       <div className="App">
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/helper">Post a Request</Link>
-            </li>
-            <li>
-              <Link to="/helperhelpers">Help a Helper</Link>
-            </li>
-          </ul>
+          <div className="menu">
+            <Link to="/" className="menu-item">
+              Home
+            </Link>
+
+            <Link to="/helper" className="menu-item">
+              Post a Request
+            </Link>
+
+            <Link to="/helperhelpers" className="menu-item">
+              Help a Helper
+            </Link>
+          </div>
         </nav>
         <Switch>
           <Route path="/helper">
@@ -27,7 +29,7 @@ function App() {
             <div>Helper Helpers</div>
           </Route>
           <Route path="/">
-            <Header />
+            <Home />
           </Route>
         </Switch>
       </div>
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.css";
+import logo from "../images/logo.png";
+
+export function Home() {
+  return (
+    <div>
+      <img className="header-logo" src={logo} />
+      <div className="about-page">
+        <h2>About</h2>
+        <p>
+          We're commited to finding ways to make a difference during these
+          difficult times. Essential workers are more overworked than ever, and
+          it's easy to feel lost and powerless as a non-essential worker during
+          these times. HelperHelper is a community that pairs essential workers
+          (Helpers) with volunteers (Helper Helpers) to reduce the burden on
+          essential workers and empower volunteers to make a difference.{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
