934239d0acd656c6bae5a942a79e214c11826c6d
Kira <kawoods826@gmail.com>
2020-06-09 10:44:27 -0700

    updates styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./Footer.css";
 
 export function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div>
+        Copyright 2020 - Kira Woods - Created for Girls Who Code Hackathon
+      </div>
+    </div>
+  );
 }
