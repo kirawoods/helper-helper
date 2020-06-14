0852e939be5a46862daecc99c54cde7a23e4cfe2
Kira <kawoods826@gmail.com>
2020-06-14 11:16:50 -0700

    adds multiple different requests for helper cards

--- src/components/HelperHelper.js
+++ src/components/HelperHelper.js
@@ -2,50 +2,54 @@ import React from "react";
 import "./HelperHelper.css";
 import { HelpRequestCard } from "./HelpRequestCard";
 
-const helpRequest = {
-  location: "San Francisco, CA",
-  request: "Need help getting groceries",
-  name: "Abby Friedman",
-};
+const helpRequests = [
+  {
+    location: "San Francisco, CA",
+    request: "Need help getting groceries",
+    name: "Ada Lovelace",
+  },
+  {
+    location: "San Mateo, CA",
+    request: "Childcare on Mondays and Wednesdays",
+    name: "Katherine Johson",
+  },
+  {
+    location: "Remote",
+    request: "Tax Preparation Services",
+    name: "Grace Hopper",
+  },
+  {
+    location: "Oakland, CA",
+    request: "House Painting",
+    name: "Margaret Hamilton",
+  },
+  {
+    location: "Remote",
+    request: "Planning Birthday Party for my 6 year old who likes horses",
+    name: "Pam Hardt-English",
+  },
+  {
+    location: "Belmont, CA",
+    request: "Math tutoring for my 10 year old",
+    name: "Joan Margaret Winters",
+  },
+  {
+    location: "Redwood City, CA",
+    request: "Book Donations for Children's Book Exchange",
+    name: "Gwen Bell",
+  },
+];
 
 export function HelperHelper() {
   return (
     <div className="help-cards-container">
-      <HelpRequestCard
-        name={helpRequest.name}
-        location={helpRequest.location}
-        request={helpRequest.request}
-      />
-      <HelpRequestCard
-        name={helpRequest.name}
-        location={helpRequest.location}
-        request={helpRequest.request}
-      />
-      <HelpRequestCard
-        name={helpRequest.name}
-        location={helpRequest.location}
-        request={helpRequest.request}
-      />
-      <HelpRequestCard
-        name={helpRequest.name}
-        location={helpRequest.location}
-        request={helpRequest.request}
-      />
-      <HelpRequestCard
-        name={helpRequest.name}
-        location={helpRequest.location}
-        request={helpRequest.request}
-      />
-      <HelpRequestCard
-        name={helpRequest.name}
-        location={helpRequest.location}
-        request={helpRequest.request}
-      />
-      <HelpRequestCard
-        name={helpRequest.name}
-        location={helpRequest.location}
-        request={helpRequest.request}
-      />
+      {helpRequests.map((request) => (
+        <HelpRequestCard
+          location={request.location}
+          request={request.request}
+          name={request.name}
+        />
+      ))}
     </div>
   );
 }
